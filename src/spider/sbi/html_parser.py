14d24eea6db4673a0c220a0b4c8381830f9c7886
keita.oouchi <keita.oouchi@gmail.com>
2020-02-07 18:58:06 +0900

    Fixed a bug with parse fund amount

--- src/spider/sbi/html_parser.py
+++ src/spider/sbi/html_parser.py
@@ -23,7 +23,7 @@ class SummaryParser:
             logging.warn("株式が見つかりませんでした")
             shares = 0
         try:
-            e = bs.find("font", text=re.compile("投資信託")).find_parent("tr").find("font",attrs={"color": "black"})
+            e = bs.find("div", text=re.compile("投資信託")).find_parent("td").find_next_sibling('td').find('div')
             trust = e.string.strip().replace(",", "")
         except:
             logging.warn("投資信託が見つかりませんでした")
